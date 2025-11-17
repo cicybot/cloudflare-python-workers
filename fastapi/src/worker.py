8b74acc9b33952e1592ad6310e93cdca955b9a7b
cicybot <cicybot@outllok.com>
2025-11-17 13:38:14 +0630

    ci

--- fastapi/src/worker.py
+++ fastapi/src/worker.py
@@ -6,10 +6,12 @@ class Default(WorkerEntrypoint):
     async def fetch(self, request):
         import asgi
         env = self.env
+        
         Global.init_cloudflare({
             "CLOUDFLARE_API_TOKEN":env.CLOUDFLARE_API_TOKEN,
             "CLOUDFLARE_ACCOUNT_ID":env.CLOUDFLARE_ACCOUNT_ID,
             "CLOUDFLARE_DATABASE_ID":env.CLOUDFLARE_DATABASE_ID,
+            "JWT_SECRET_KEY":env.JWT_SECRET_KEY,
             "CLOUDFLARE_KV_NAMESPACE_ID":env.CLOUDFLARE_KV_NAMESPACE_ID
         })
         return await asgi.fetch(app, request.js_object, self.env)
