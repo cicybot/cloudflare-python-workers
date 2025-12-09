84b95b960c4894377ea42970ff1c5ae31db635ef
cicybot <cicybot@outllok.com>
2025-12-09 12:06:36 +0630

    add crypto

--- fastapi/src/routers/personal.py
+++ fastapi/src/routers/personal.py
@@ -1,7 +1,7 @@
 import logging
 from fastapi import APIRouter, Form
-from common import crypto
-
+from common import crypto,utils
+import os
 import requests
 
 from service.Global import Global
@@ -21,14 +21,29 @@ async def info(pwd: str = Form(...),info_url: str = Form(None)):
         info_url = Global.get_options("INFO_URL")
     response = requests.get(info_url)
     return {
-        "info": crypto.aes_decrypt(pwd,response.text),
+        "info": crypto.aes_decrypt(pwd,response.text).split("\n"),
     }
 
-@router.get("/encrypt")
-async def info():
+@router.post("/encrypt")
+async def encrypt(pwd: str = Form(...)):
     if Global.get_options("is_cf") is not None:
         return {}
 
+    # if pwd.startswith("Pwd@") is False:
+    #     return {
+    #         "err":"password is not valid"
+    #     }
+
+    file_info_data = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../assets")),"info.data")
+    file_info_data_plain = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../assets")),"info.data.log")
+    if utils.file_exists(file_info_data_plain) is False:
+        return {"err":"not found file"}
+    data_info = utils.file_get_content(file_info_data_plain)
+    utils.file_put_content(file_info_data,crypto.aes_encrypt(pwd,data_info))
+
+    plain = crypto.aes_decrypt(pwd,utils.file_get_content(file_info_data))
+
     return {
-        # "info": crypto.aes_encrypt(pwd,response.text),
+        "plain":plain.split("\n"),
+        'file':utils.file_get_content(file_info_data),
     }
